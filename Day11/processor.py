2a5d7b615aaeee982e12c3b4538f0dd09548b338
Andrew Hutcheson <andrew.hutcheson@gmail.com>
2020-12-12 13:30:18 -0600

    Fix day11

--- Day11/processor.py
+++ Day11/processor.py
@@ -64,5 +64,75 @@ for n in originalSeats.values():
     if n == "#":
         count += 1
 
-print(iterationCount)    
-print(count)
+print("Part 1 iterations:",iterationCount)    
+print("Part 1:", count)
+
+###############################################
+################### part 2 ####################
+###############################################
+
+#ok, now I want a function that keeps searching in that same direction.
+def isDirectionOccupied(x,y,seats,offset):
+    while True:
+        x = x + offset[0]
+        y = y + offset[1]
+
+        try:
+            if(seats[x,y]=="#"):
+                return True
+            elif(seats[x,y]=="L"):
+                return False
+            else: #if its == ".", do the loop again and increment in that direction
+                pass
+        except KeyError:
+            return False
+
+#repopulate input
+originalSeats = {}
+for r, row in enumerate(seatRows):
+    for c, seat in enumerate(row):
+        originalSeats[r,c] = seat
+
+newSeats = {}
+changed = True
+iterationCount = 0
+while changed:
+    for (row, col), seat in originalSeats.items():
+        #print("row",row,"col",col)
+        if seat == ".":
+            newSeats[row,col] = "."
+        elif seat == "L":
+            occupied = 0
+            for i in range(-1,2): #-1,2 gives -1 to 1
+                for j in range(-1,2):
+                    if (i == 0) and (j == 0):
+                        continue
+                    if(isDirectionOccupied(row,col,originalSeats,[i,j])):
+                        occupied += 1
+            if(occupied == 0):
+                newSeats[row,col] = "#"
+        elif(seat == "#"):
+            occupied = 0
+            for i in range(-1,2): #-1,2 gives -1 to 1
+                for j in range(-1,2):
+                    if (i == 0) and (j == 0):
+                        continue
+                    if(isDirectionOccupied(row,col,originalSeats,[i,j])):
+                        occupied += 1
+            if(occupied >= 5):
+                newSeats[row,col] = "L"
+    
+    if(newSeats == originalSeats):
+        changed = False
+
+    originalSeats = newSeats.copy()
+    iterationCount += 1
+    #print(iterationCount)
+
+count = 0
+for n in originalSeats.values():
+    if n == "#":
+        count += 1
+
+print("Part 2 iterations:",iterationCount)    
+print("Part 2:", count)
