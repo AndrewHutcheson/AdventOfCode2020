4f74825305c50d35bbf96866847fbf6f1c42f7ef
Andrew Hutcheson <andrew.hutcheson@gmail.com>
2020-12-16 13:20:38 -0600

    Day16

---
+++ Day16/processor.py
@@ -0,0 +1,94 @@
+stream = open("input.txt")
+lines = stream.read()
+data = lines.split("\n")
+#there's sometimes an empty newline at the end
+for row in data:
+    if(row == ""):
+        data.remove(row)
+
+#rules
+validRanges = []
+
+for row in data:
+    line = row.split(": ")
+    #stop after the rules
+    if(line[0] == "your ticket:"):
+        break
+    ranges = line[1].split(" or ")
+    for subRange in ranges:
+        subSubRange = subRange.split("-")
+        
+        startNum = subSubRange[0]
+        endNum = subSubRange[1]
+        validRanges.append([startNum,endNum])
+
+#now find all invalid tickets, ignoring my own
+startFromHere = False
+otherTickets = []
+for row in data:
+    line = row.split(": ")
+    if(line[0]) == "nearby tickets:":
+        startFromHere = True
+    elif(startFromHere):
+        otherTickets.append(row)
+
+onlyValidTickets = []
+errorRate = 0
+numInvalidTickets = 0
+for ticket in otherTickets:
+    allTicketValues = True
+    values = ticket.split(",")
+    for value in values:
+        found = False
+        for testRange in validRanges:
+            if (int(value) <= int(testRange[1])) and (int(value) >= int(testRange[0])): #not casting int yields false string comparisons
+                found = True
+        if found == False:
+            allTicketValues = False
+            errorRate += int(value)
+    if(allTicketValues == False):
+        numInvalidTickets += 1
+    else:
+        onlyValidTickets.append(ticket)
+
+print("part 1",errorRate)
+
+#next identify the fields. 
+#for each field, loop over just the first value in each ticket. if all are valid i have IDed it. if not, move on to second field.
+departureRanges = {}
+departureRangeRowIds = {}
+for row in data:
+    line = row.split(": ")
+    #stop after the rules
+    if(line[0] == "your ticket:"):
+        break
+    ranges = line[1].split(" or ")
+    subrange0 = ranges[0].split("-")
+    subrange1 = ranges[1].split("-")
+        
+    startNum0 = subrange0[0]
+    endNum0 = subrange0[1]
+    startNum1 = subrange1[0]
+    endNum1 = subrange1[1]
+    if(line[0].find("departure") != -1):
+        departureRanges[line[0]] = ([startNum0,endNum0,startNum1,endNum1])
+        departureRangeRowIds[line[0]] = []
+
+def isThisTheRightColumn(checkRange,validTickets,fieldNumber):
+    returnValue = True
+    for row in validTickets:
+        numbers = row.split(",")
+        if(int(checkRange[0]) <= int(numbers[fieldNumber]) <= int(checkRange[1])) or (int(checkRange[2]) <= int(numbers[fieldNumber]) <= int(checkRange[3])):
+            pass
+        else:
+            returnValue = False
+    return returnValue
+
+for departureItem in departureRanges:
+    columnToCheck = 0
+    while columnToCheck <= 19:
+        if(isThisTheRightColumn(departureRanges[departureItem],onlyValidTickets,columnToCheck)):
+            departureRangeRowIds[departureItem].append(columnToCheck)
+        columnToCheck += 1
+
+print(departureRangeRowIds)
