ff33514320ef3c745baea8c0b44fe23588c59ae9
Andrew Hutcheson <andrew.hutcheson@gmail.com>
2020-12-24 00:29:22 -0600

    part1 complete

--- Day22/processor.py
+++ Day22/processor.py
@@ -1,4 +1,5 @@
 stream = open("input.txt")
+#stream = open("sampleInput.txt")
 lines = stream.read()
 data = lines.split("\n")
 #there's sometimes an empty newline at the end
@@ -22,18 +23,18 @@ for row in data:
 while (len(player1) > 0) and (len(player2) > 0):
     print("player 1's deck:",player1)
     print("player 2's deck:",player2)
-    player1Card = int(player1.pop(len(player1)-1))
-    player2Card = int(player2.pop(len(player2)-1))
+    player1Card = int(player1.pop(0))
+    player2Card = int(player2.pop(0))
     print("player1 plays:",player1Card)
     print("player2 plays:",player2Card)
     if player1Card > player2Card:
         print("player1 wins")
-        player1.insert(0,player1Card)
-        player1.insert(0,player2Card)
+        player1.insert(len(player1),player1Card)
+        player1.insert(len(player1),player2Card)
     else:
         print("player2 wins")
-        player2.insert(0,player2Card)
-        player2.insert(0,player1Card)
+        player2.insert(len(player2),player2Card)
+        player2.insert(len(player2),player1Card)
     print()
 
 if(len(player1) > len(player2)):
@@ -43,9 +44,10 @@ else:
 
 score = 0
 index = 0
+multiplier = len(winner)
 while index < len(winner):
-    score += (index+1) * int(winner[index])
-    print(index+1,"*",winner[index],"=",(index+1)*int(winner[index]))
+    score += multiplier * int(winner[index])
+    print(multiplier,"*",winner[index],"=",multiplier*int(winner[index]))
     index += 1
-
+    multiplier -= 1
 print(score)
